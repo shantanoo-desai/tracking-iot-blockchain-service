3f803e126f103d1dda1dd8f826783a30c303b436
Shantanoo Desai <des@biba.uni-bremen.de>
2019-11-14 11:41:46 +0100

    Adapt code for uWSGI server + documentation+lint
    - the uWSGI requires the file name (here, `app.py`)
       as well as the module which provides the
       Flask instance (here, `entrypoint`)
    - Function Documentation
    - Flake8 Linting

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-import os
 import logging
-import urllib.parse
 from flask import Flask, Blueprint
 from flask_cors import CORS
 from apis import api
@@ -9,15 +7,24 @@ from databases import mongo, influx
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-app = Flask(__name__)
-CORS(app)
 
 def configure_app(flask_app):
-    # Flask-Restplus Swagger Configuration
+    '''Configure the complete Flask App using a `.cfg` file
+       as an environment variable
+    '''
+
     logger.info('Configuring Flask App')
-    flask_app.config.from_envvar('APP_CONFIG')
+    try:
+        flask_app.config.from_envvar('APP_CONFIG')
+    except Exception as e:
+        logger.error('No APP_CONFIG variable found. No configuration found.')
+        raise(e)
+
 
 def initialize_app(flask_app):
+    '''Initialize the complete Flask App with a Blueprint
+       and Database initializations
+    '''
     configure_app(flask_app)
 
     blueprint = Blueprint('api', __name__, url_prefix='/api')
@@ -28,10 +35,20 @@ def initialize_app(flask_app):
     mongo.init_app(flask_app)
     influx.init_app(flask_app)
 
+
 def main():
+    '''Main function that returns the CORS-configured Flask App
+    '''
     logger.info('Starting Server for IoT-Blockchain API')
+    app = Flask(__name__)
+    CORS(app)
     initialize_app(app)
-    app.run(debug=False, host='0.0.0.0')
+    return app
+
+
+# Necessary for uWSGI Server's configuration file
+entrypoint = main()
 
 if __name__ == "__main__":
-    main()
+    # Always in Production Mode.
+    entrypoint.run(debug=False, host='0.0.0.0')
