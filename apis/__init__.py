5ea46d07b56155c551ff13e09675ad9a5ed3d19b
Shantanoo Desai <des@biba.uni-bremen.de>
2019-11-20 16:19:27 +0100

    Bump v1.2 -> v1.3
    - Structural changes based on Blueprint for APIs

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,17 +1,23 @@
 # Main API Declaration for IoT-Blockchain API
 # All different Namespaces for the App are imported here
 
+from flask import Blueprint
 from flask_restplus import Api
 
 from .sensordoc import api as sensordoc_ns
 from .hashdoc import api as hashdoc_ns
 from .verification import api as verification_ns
 
+blueprint = Blueprint('api', __name__)
+
 api = Api(
+    blueprint,
     title='IoT-Blockchain API',
-    version='1.2',
+    version='1.3',
     description='IoT Data and Blockchain related Information for NIMBLE Platform Track&Trace',
-    doc='/doc/'
+    doc='/doc/',
+    contact='Shantanoo Desai',
+    contact_email='des@biba.uni-bremen.de',
 )
 
 api.add_namespace(sensordoc_ns)
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import logging
-from flask import Flask, Blueprint
+from flask import Flask
 from flask_cors import CORS
-from apis import api
+from apis import blueprint as api
 from databases import mongo, influx
 
 logging.basicConfig(level=logging.DEBUG)
@@ -27,9 +27,9 @@ def initialize_app(flask_app):
     '''
     configure_app(flask_app)
 
-    blueprint = Blueprint('api', __name__, url_prefix='/api')
-    api.init_app(blueprint)
-    flask_app.register_blueprint(blueprint)
+    # blueprint = Blueprint('api', __name__, url_prefix='/api')
+    # api.init_app(blueprint)
+    flask_app.register_blueprint(api, url_prefix='/api')
 
     # Databases (Mongo, InfluxDB)
     mongo.init_app(flask_app)
@@ -51,4 +51,4 @@ entrypoint = main()
 
 if __name__ == "__main__":
     # Always in Production Mode.
-    entrypoint.run(debug=False, host='0.0.0.0')
+    entrypoint.run(debug=False)
